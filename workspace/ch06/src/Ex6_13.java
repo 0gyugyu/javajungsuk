cf9e680d2643e01b7c3ab882f1ca6d5b9983161f
0gyugyu <dudrbrla97@gmail.com>
2024-05-02 02:53:43 +0900

    javajungsuk

--- workspace/ch06/src/Ex6_13.java
+++ workspace/ch06/src/Ex6_13.java
@@ -26,11 +26,11 @@ public class Ex6_13 {
     public static void main(String[] args) {
         Car2 c1 = new Car2();
         Car2 c2 = new Car2("blue");
-        Car2 c3 = new Car2("pink", "manual", 5);
+        //Car2 c3 = new Car2("pink", "manual", 5);
 
         System.out.println("c1의 color=" + c1.color + ", gearType=" + c1.gearType+ ", door="+c1.door);
         System.out.println("c2의 color=" + c2.color + ", gearType=" + c2.gearType+ ", door="+c2.door);
-        System.out.println("c3의 color=" + c3.color + ", gearType=" + c3.gearType+ ", door="+c3.door);
+        //System.out.println("c3의 color=" + c3.color + ", gearType=" + c3.gearType+ ", door="+c3.door);
 
     }
 
